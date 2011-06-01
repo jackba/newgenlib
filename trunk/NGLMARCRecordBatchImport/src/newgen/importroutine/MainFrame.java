938bc5e3202feb1604420a32717d64ccc043f0ca
emsiddartha@gmail.com <emsiddartha@gmail.com@8c50a735-a96b-aa54-8039-24d2602cefd9>
2011-06-01 05:58:31 +0000

    commit

--- trunk/NGLMARCRecordBatchImport/src/newgen/importroutine/MainFrame.java
+++ trunk/NGLMARCRecordBatchImport/src/newgen/importroutine/MainFrame.java
@@ -11,7 +11,9 @@
 package newgen.importroutine;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.RandomAccessFile;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
@@ -45,6 +47,7 @@ public class MainFrame extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
@@ -80,16 +83,15 @@ public class MainFrame extends javax.swing.JFrame {
 
         getContentPane().add(jPanel1);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 444, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
+        jPanel2.setLayout(new java.awt.GridBagLayout());
+
+        jButton3.setText("Import the above file");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton3, new java.awt.GridBagConstraints());
 
         getContentPane().add(jPanel2);
 
@@ -122,12 +124,30 @@ public class MainFrame extends javax.swing.JFrame {
                         recordCount += count;
                     }
                 }
-                JOptionPane.showConfirmDialog(null, "Found " + recordCount + " records", "Records count", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Found " + recordCount + " records", "Records count", JOptionPane.INFORMATION_MESSAGE);
                 return "";
             }
         };
         sw.execute();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        try {
+            RandomAccessFile raf = new RandomAccessFile(new File(jTextField1.getText()), "r");
+            long len = raf.length();
+            raf.seek(0);
+            for (int i = 0; i < len; i++) {
+                char ch = raf.readChar();                
+                System.out.println(ch);
+            }
+            
+            raf.close();
+        } catch (Exception e) {
+        }
+        
+        
+    }//GEN-LAST:event_jButton3ActionPerformed
     public int countOccurrences(String haystack, char needle) {
         int count = 0;
         for (int i = 0; i < haystack.length(); i++) {
@@ -152,6 +172,7 @@ public class MainFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
